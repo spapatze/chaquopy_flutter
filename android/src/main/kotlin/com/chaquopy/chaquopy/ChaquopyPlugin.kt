404c572145aa27b81f97315a0f2e66815b253b69
spapatze <spypaptz@gmail.com>
2022-07-18 13:46:02 +0300

    Update

--- android/src/main/kotlin/com/chaquopy/chaquopy/ChaquopyPlugin.kt
+++ android/src/main/kotlin/com/chaquopy/chaquopy/ChaquopyPlugin.kt
@@ -49,7 +49,7 @@ class ChaquopyPlugin : FlutterPlugin, MethodCallHandler {
         if (call.method == "runPythonScript") {
             try {
                 val code: String? = call.arguments()
-                val _result: Map<String, Any?> = _runPythonTextCode(code)
+                val _result: Map<String?, Any?> = _runPythonTextCode(code)
                 result.success(_result)
             } catch (e: Exception) {
                 val _result: MutableMap<String, Any?> = HashMap()
