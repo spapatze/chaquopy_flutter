f4b76207ecdd7446e84cbf392e6a533553b07a86
spapatze <spypaptz@gmail.com>
2022-07-19 12:12:41 +0300

    Update

--- lib/chaquopy.dart
+++ lib/chaquopy.dart
@@ -9,8 +9,8 @@ class Chaquopy {
   /// This function execute your python code and returns result Map.
   /// Structure of result map is :
   /// result['textOutput'] : The original output / error
-  static Future<Map<String, dynamic>> executeCode(String code) async {
+  static Future<String> executeCode(String code) async {
     dynamic outputData = await _channel.invokeMethod('runPythonScript', code);
-    return Map<String, dynamic>.from(outputData);
+    return outputData;
   }
 }
